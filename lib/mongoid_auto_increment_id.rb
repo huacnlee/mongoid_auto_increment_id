ce520ec79ce322cfb9dd805cfee08aa06689613f
Jason Lee <huacnlee@gmail.com>
2011-09-20 15:23:04 +0800

    Fix User.find(1) != User.find(1) issue #2

--- lib/mongoid_auto_increment_id.rb
+++ lib/mongoid_auto_increment_id.rb
@@ -15,7 +15,7 @@ module Mongoid
     included do
       identity :type => String
     end
-        
+
     def identify
     end
     
@@ -26,6 +26,31 @@ module Mongoid
         result["_id"] = Identity.new(self).generate_id
       end
       result
+    end
+  end
+  
+  module Extensions #:nodoc:
+    module ObjectId #:nodoc:
+      module Conversions
+        def convert(klass, args, reject_blank = true)
+          case args
+          when ::Array
+            args.delete_if { |arg| arg.blank? } if reject_blank
+            args.replace(args.map { |arg| convert(klass, arg, reject_blank) })
+          when ::Hash
+            args.tap do |hash|
+              hash.each_pair do |key, value|
+                next unless klass.object_id_field?(key)
+                begin
+                  hash[key] = convert(klass, value, reject_blank)
+                rescue BSON::InvalidObjectId; end
+              end
+            end
+          else
+            args.to_s
+          end
+        end
+      end
     end
   end
 end
